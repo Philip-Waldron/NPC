2265d46ce1181d1424e9c573a358395844dafe7a
Philip-Waldron <philip_waldron@yahoo.com>
2019-08-16 02:05:07 +0100

    Add some spicy pathfinding things

---
+++ Assets/AstarPathfindingProject/Modifiers/RandomPenaltySetter.cs
@@ -0,0 +1,25 @@
+﻿using Pathfinding;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class RandomPenaltySetter : MonoBehaviour
+{
+    [SerializeField]
+    private float _minPenalty = 0;
+    [SerializeField]
+    private float _maxPenalty = 100000;
+    
+    private GridNode[] _gridNodes;
+
+    private void Start()
+    {
+        _gridNodes = AstarPath.active.data.gridGraph.nodes;
+        AstarPath.active.AddWorkItem(() =>
+        {
+            foreach (var node in _gridNodes)
+            {
+                node.Penalty = (uint) Random.Range(_minPenalty, _maxPenalty);
+            }
+        });
+    }
+}
